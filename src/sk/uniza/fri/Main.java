6a007d139044fb8920cf3091f38c02bc8573a903
Alexander Brezáni <alexander.brezani@gmail.com>
2022-03-14 13:22:39 +0100

    Project Upload
    Project Upload

---
+++ src/sk/uniza/fri/Main.java
@@ -0,0 +1,14 @@
+package sk.uniza.fri;
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alex-PC
+ * Date: 14. 3. 2022
+ * Time: 12:47
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO Write your code
+        System.out.println("Hello world");
+    }
+}
